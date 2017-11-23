367c2d6f1dbfab90aeb084ba42b68f0bca4cc69c
Toprak Koç <toprakademkoc360@gmail.com>
2017-11-23 16:12:42 +0300

    Update
    Update

--- save.php
+++ save.php
@@ -75,7 +75,6 @@ function type($str)
 function mime($str)
 {
 	$neverAllow = array(
-
 		"application/octet-stream",
 		"application/javascript",
 		"text/javascript"
@@ -95,7 +94,6 @@ $formid = injection($_POST["formid"]);
 $qid = injection($_POST["qid"]);
 $path = DIRECTORY_SEPARATOR . "tmp"; 
 $file_path = implode(DIRECTORY_SEPARATOR, array($path,$formid,"questionid".$qid));
-
 if(realpath($file_path) !== true)
 {
 	if(file_exists($file_path) !== true)
@@ -103,7 +101,6 @@ if(realpath($file_path) !== true)
 		mkdir($file_path, 777,true);
 	}
 }
-
 foreach ($_FILES as $key => $value) {
 	$file_name = injection($_FILES[$key]["name"]);
 	$array = explode('.', $file_name);
@@ -113,10 +110,8 @@ foreach ($_FILES as $key => $value) {
 		exit(json_encode(array("succes"=>false,"error"=>"type")));
 	}
 	
-
 	$chars = range("a","z");
 	$numbers= range("0","9");
-
 	foreach ($chars as $char){
 		if(stripos($file_name, $char)){
 			break;		
@@ -137,7 +132,6 @@ foreach ($_FILES as $key => $value) {
 		{
 			exit(json_encode(array("succes"=>false,"error"=>"mime_content_type(filename)")));	
 		}
-
 		chmod($file_path. DIRECTORY_SEPARATOR .$file_name, 0777);
 		header("HTTP/1.1 200");
 		die(json_encode(array("succes"=>true,"error"=>null)));			
