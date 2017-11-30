88978600b5e6cc625220f518d6cd5b7dbfc469a6
Toprak Koç <toprakademkoc360@gmail.com>
2017-11-30 10:22:08 +0300

    Trying to get refresh and access token
    Trying to get refresh and access token

--- test.php
+++ test.php
@@ -1,12 +1,20 @@
 <?
-$code = $_POST["code"];
 require_once __DIR__.'/vendor/autoload.php';
 
 
-session_start();
+// session_start();
 
+$code = explode('"',$_POST["code"]);
+$del = array('"',"{","}","code",":");
+do{
+	$old = $code;
+	$code = str_replace($del,"",$code);
+	$code = array_filter($code);
+}
+while($old !== $code);
+$code = implode($code);
 $client = new Google_Client();
 $client->authenticate($code);
-var_dump($client->getAccessToken());
+$client->getAccessToken();
 $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/';
 header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
