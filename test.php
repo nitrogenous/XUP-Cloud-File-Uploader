270d627ef2ba20d88da47a818e677aa82700b7b6
Toprak Koç <toprakademkoc360@gmail.com>
2017-11-30 09:54:29 +0300

    Drive Adapter Testing

--- test.php
+++ test.php
@@ -1,19 +1,12 @@
 <?
+$code = $_POST["code"];
 require_once __DIR__.'/vendor/autoload.php';
+
 
 session_start();
 
 $client = new Google_Client();
-$client->setAuthConfigFile('client_secrets.json');
-$client->setRedirectUri('https://' . $_SERVER['HTTP_HOST'] . '/Adapter/test.php');
-$client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
-
-if (! isset($_GET['calculhmac(clent, data)ode'])) {
-	$auth_url = $client->createAuthUrl();
-	header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
-} else {
-	$client->authenticate($_GET['code']);
-	$_SESSION['access_token'] = $client->getAccessToken();
-	$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/';
-	header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
-}
+$client->authenticate($code);
+var_dump($client->getAccessToken());
+$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/';
+header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
