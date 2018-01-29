73ddbfca24ef94d2ba4669072615202bf58ba190
Toprak Koç <toprakademkoc360@gmail.com>
2018-01-29 17:51:38 +0300

    added upload and remove functions

--- file.php
+++ file.php
@@ -8,20 +8,61 @@ use XUP\Uploader\Main;
 use XUP\Uploader\Drive;
 use XUP\Uploader\Dropbox;
 use XUP\Uploader\AmazonWebServices;
-$output
+$output = array();
 $services = array("Drive","Dropbox","AmazonWebServices");
 $action = $_POST["action"];
-foreach ($services as $service => $value) {
+foreach ($services as $service) {
 	$class = "XUP\Uploader\\".$service;
 	$adapter = new $class();
-	$output = $action($_POST);
+	$output[$service] = $action($adapter,$_POST);
 }
 exit(json_encode($output));
 
-function upload($post,$value) {
-	$value =  "XUP\Uploader\\".$value;
-	$adapter = new $value();
-	$params = json_encode(array("formid" => $this->injection($post["formid"]),"folder"=>$this->injection($post["folder"]),"qid" => $this->injection($post["qid"]), "key" => $post["key"], "file" => $this->injection($post["file"]),"folderKey" => $this->injection($post["folderKey"])));
-
+function upload($adapter,$post) {
+	$params = json_encode(array("formid" => injection($post["formid"]),"folder"=>injection($post["folder"]),"qid" => injection($post["qid"]), "key" => $post["key"], "file" => injection($post["file"]),"folderKey" => injection($post["folderKey"])));
 	return $adapter->upload($params);
+}
+function deleteFile($adapter,$post) {
+	$params = json_encode(array("formid" => injection($post["formid"]),"qid" => injection($post["qid"]),"remove" => $post["remove"],"aws" => $post["aws"]));
+	return $adapter->deleteFile($params);
+}
+
+function injection($str) {
+	$bad = array(
+		'<!--', '-->',
+		"'", '"',
+		'<', '>',
+		'&', '$',
+		'=',
+		';',
+		'?',
+		'/',
+		'!',
+		'#',
+		'%20',		//space
+		'%22',		// "
+		'%3c',		// <
+		'%253c',	// <
+		'%3e',		// >
+		'%0e',		// >
+		'%28',		// (
+		'%29',		// )
+		'%2528',	// (
+		'%26',		// &
+		'%24',		// $
+		'%3f',		// ?
+		'%3b',		// ;
+		'%3d',		// =
+		'%2F',		// /
+		'%2E',		// .
+		// '46', 		// .
+		// '47'		// /
+	);
+	do
+	{
+		$old = $str;
+		$str = str_replace($bad, ' ', $str);
+	}
+	while ($old !== $str);
+	return $str;	
 }
