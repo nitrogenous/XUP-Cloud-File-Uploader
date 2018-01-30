e50a7bad1e2658ccf4584ffaafb3c457e0d160df
Toprak Koç <toprakademkoc360@gmail.com>
2018-01-30 15:51:12 +0300

    removed space

--- database.php
+++ database.php
@@ -5,7 +5,6 @@ require_once(__DIR__.DIRECTORY_SEPARATOR."src".DIRECTORY_SEPARATOR."XUP".DIRECTO
 require_once(__DIR__.DIRECTORY_SEPARATOR."src".DIRECTORY_SEPARATOR."XUP".DIRECTORY_SEPARATOR."adapters".DIRECTORY_SEPARATOR."amazonwebservices.php");
 require_once(__DIR__.DIRECTORY_SEPARATOR."functions.php");
 
-
 use XUP\Uploader\Main;
 use XUP\Uploader\Drive;
 use XUP\Uploader\Dropbox;
